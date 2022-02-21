84682d73747f9b683f8e902d5913abf2f43d5097
wgbailyr <100082373+wgbailyr@users.noreply.github.com>
2022-02-21 21:49:31 +0800

    Add files via upload
    add a files

---
+++ demos1.py
@@ -0,0 +1,10 @@
+def logger(func):
+    def wrapper():
+        print('Logging execution')
+        func()
+        print('Done logging')
+    return wrapper()
+
+@logger
+def sample():
+    print('--Inside sample function')
